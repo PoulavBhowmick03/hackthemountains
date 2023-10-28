502f97746e41eb5b15c46e32c51340061a057d2d
Poulav Bhowmick <bpoulav@gmail.com>
2023-10-29 04:43:30 +0530

    changes

--- src/app/dash/form/page.jsx
+++ src/app/dash/form/page.jsx
@@ -1,6 +1,8 @@
 "use client";
 
 import React, { useState } from 'react';
+import Image from "next/image";
+import form from "public/form.png";
 
 const HealthProblemForm = () => {
   const [healthProblem, setHealthProblem] = useState('');
@@ -39,39 +41,48 @@ const HealthProblemForm = () => {
   };
 
   return (
-    <div className="w-full max-w-md mx-auto p-4 bg-white rounded-lg shadow-md">
-      <h2 className="text-2xl font-semibold text-center mb-4">Health Problem Form</h2>
-      <form onSubmit={handleSubmit}>
-        <div className="mb-4">
-          <label htmlFor="healthProblem" className="block text-gray-700 font-bold">
-            Current Health Problem:
-          </label>
-          <input
-            type="text"
-            id="healthProblem"
-            name="healthProblem"
-            value={healthProblem}
-            onChange={(e) => setHealthProblem(e.target.value)}
-            className="w-full px-4 py-2 border border-gray-300 rounded focus:outline-none focus:ring focus:border-blue-400 text-black"
-            placeholder="Enter your current health problem"
-          />
-        </div>
-        <button
-          type="submit"
-          className="w-full bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded focus:outline-none focus:ring"
-        >
-          Submit
-        </button>
-      </form>
+    <div>
+<div className="flex">
+  <div className="mx-20 w-full max-w-md p-3 bg-white rounded-lg shadow-md">
+    <h2 className="text-2xl font-semibold text-center mb-4">Health Problem Form</h2>
+    <form onSubmit={handleSubmit} className="max-w-md mx-auto p-4 bg-white shadow-md rounded-lg">
+      <div className="mb-4">
+        <label htmlFor="healthProblem" className="block text-gray-700 text-sm font-bold mb-2">
+          Current Health Problem:
+        </label>
+        <input
+          type="text"
+          id="healthProblem"
+          name="healthProblem"
+          value={healthProblem}
+          onChange={(e) => setHealthProblem(e.target.value)}
+          className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring focus:border-blue-400 text-gray-700"
+          placeholder="Enter your current health problem"
+        />
+      </div>
+      <button
+        type="submit"
+        className="w-full bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-lg focus:outline-none focus:ring"
+      >
+        Submit
+      </button>
+    </form>
 
-      {/* Display the API response */}
-      {apiResponse && (
-        <div className="mt-8 bg-gray-100 p-4 rounded-lg">
-          <h3 className="text-xl font-semibold mb-2">API Response:</h3>
-          <pre className="text-gray-800 whitespace-pre-wrap">{JSON.stringify(apiResponse, null, 2)}</pre>
-        </div>
-      )}
-    </div>
+    {/* Display the API response */}
+    {apiResponse && (
+      <div className="mt-8 bg-gray-100 p-4 rounded-lg">
+        <h3 className="text-xl font-semibold mb-2">API Response:</h3>
+        <pre className="text-gray-800 whitespace-pre-wrap">{JSON.stringify(apiResponse, null, 2)}</pre>
+      </div>
+    )}
+  </div>
+
+  <div className="flex-1">
+    <Image src={form} alt="" className="w-full h-96 object-contain" />
+  </div>
+</div>
+
+  </div>
   );
 };
 
