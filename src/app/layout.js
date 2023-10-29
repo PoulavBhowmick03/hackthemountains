6c415ba0887c46e4b37a66e06811c872a90d6f86
SoarinSkySagar <sagarrana.pvt@gmail.com>
2023-10-29 23:40:45 +0530

    changed title

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,7 +8,7 @@ import AuthProvider from "@/components/AuthProvider/AuthProvider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Sastho Sathi",
+  title: "Chikitsalaya",
   description: "This is the description",
 };
 
