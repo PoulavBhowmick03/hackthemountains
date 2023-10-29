cb8ddb07f120ca441abd8d02fccde249e0486302
Poulav Bhowmick <bpoulav@gmail.com>
2023-10-30 00:24:04 +0530

    dep2

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -19,24 +19,27 @@ const About = () => {
         <div className={styles.item}>
           <h1 className={styles.title}></h1>
           <p className={styles.desc}>
-          <br /><div className={styles.title}>Who Are We?</div>
+            <br />
+            <div className={styles.title}>Who Are We?</div>
             <br />
             <br />Welcome to Chikitsalay, your trusted companion on your journey to better health.
             <br />we are  passionate and committed to improving healthcare accessibility, information,
-            <br />and personalization.we are passionate about enhancing healthcare accessibility, information,
-            <br /> and personalization & helping you find the nearest health centers when you need them the most.
+            <br />and personalization. We are passionate about enhancing healthcare accessibility, information,
+            <br /> and personalization &amp; helping you find the nearest health centers when you need them the most.
           </p>
-          <br /><div className={styles.title}>Our Vision</div>
+          <br />
+          <div className={styles.title}>Our Vision</div>
           <p className={styles.desc}>
             <br />At Chikitsalay, we envision a world where healthcare is a fundamental right
             <br />accessible to all through technology and innovation.
             <br />Our platform empowers you to make informed health decisions.
           </p>
-          <br /><div className={styles.title}>Our Values</div>
+          <br />
+          <div className={styles.title}>Our Values</div>
           <p className={styles.desc}>
             <br />Accessibility: We're dedicated to universal healthcare access, irrespective of location or status.
             <br />Accuracy: We prioritize accuracy in our disease predictions to provide users with reliable information.
-            <br />Empowerment:Individuals can take control of their health with the tools and information they need.
+            <br />Empowerment: Individuals can take control of their health with the tools and information they need.
             <br />Privacy: Privacy and data security are paramount to us. We adhere to strict privacy and data protection policies to ensure your information is safe and confidential.
             <br />
             <br />Our platform empowers you to make informed health decisions.
@@ -46,22 +49,23 @@ const About = () => {
         <div className={styles.item}>
           <h1 className={styles.title}>What We Do?</h1>
           <p className={styles.desc}>
-          <br /><div className={styles.title}>Symptom-Based Disease Prediction</div>
-
-          <br />
-          <br />⦾ Chikitsalay allows users to input their symptoms.
-          <br />
-          <br />⦾ Then feeds the info into a model based on a sophisticated algorithm.
-          <br />
-          <br />⦾ It then predicts the most probable diseases associated with those symptoms.
-          <br />
-          <br />⦾ This feature can provide valuable insights into your health and help you take appropriate actions.
             <br />
-            <br /><div className={styles.title}>Nearby Health Centers Map</div>
+            <div className={styles.title}>Symptom-Based Disease Prediction</div>
+            <br />
+            <br />⦾ Chikitsalay allows users to input their symptoms.
+            <br />
+            <br />⦾ Then feeds the info into a model based on a sophisticated algorithm.
+            <br />
+            <br />⦾ It then predicts the most probable diseases associated with those symptoms.
+            <br />
+            <br />⦾ This feature can provide valuable insights into your health and help you take appropriate actions.
+            <br />
+            <br />
+            <div className={styles.title}>Nearby Health Centers Map</div>
             <br />
             <br />⦾ Chikitsalay's map feature uses your location data to get your location.
             <br />
-            <br />⦾ It then uses it to display nearby health centers, hospitals, clinics, and pharmacies. 
+            <br />⦾ It then uses it to display nearby health centers, hospitals, clinics, and pharmacies.
             <br />
             <br />⦾ This makes it easy for you to find the closest healthcare facilities.
           </p>
