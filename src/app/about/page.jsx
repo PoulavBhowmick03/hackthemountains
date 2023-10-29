42a9b2f45498278e5ade27ceb2891492084b7efe
Binayak mondal <mondalbinayak9932@gmail.com>
2023-10-29 23:17:52 +0530

    update

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -13,12 +13,7 @@ const About = () => {
           alt=""
           className={styles.img}
         />
-        <div className={styles.imgText}>
-          <h1 className={styles.imgTitle}>Digital Storytellers</h1>
-          <h2 className={styles.imgDesc}>
-            Handcrafting award winning digital experiences
-          </h2>
-        </div>
+        
       </div>
       <div className={styles.textContainer}>
         <div className={styles.item}>
